600afa5bdee8f412ae8d28558ad4278c89f5cccb
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-03-13 18:37:35 +0900

    feat: 솔방울 프로젝트 생성

---
+++ solbangul/src/main/java/com/solbangul/SolbangulApplication.java
@@ -0,0 +1,13 @@
+package com.solbangul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolbangulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolbangulApplication.class, args);
+	}
+
+}
