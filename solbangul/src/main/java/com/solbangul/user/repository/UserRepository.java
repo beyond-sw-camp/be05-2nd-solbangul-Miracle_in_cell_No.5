4ef8a0132ad80104107a585a83fb2c5cf17cd4f0
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-03-14 17:35:10 +0900

    feat: User 엔티티, UserRepository 스프링 데이터 JPA, UserRepository 테스트 추가

---
+++ solbangul/src/main/java/com/solbangul/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.solbangul.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.solbangul.user.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
